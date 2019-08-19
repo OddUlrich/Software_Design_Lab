95c9a473db2d427dcfbb80a913d0f282561476e1
OddUlrich <vmoonodd@gmail.com>
2019-08-19 23:42:05 +1000

    Finished all PartC.

--- comp2100-labs/labtest1/PartC/QC2/Parser.java
+++ comp2100-labs/labtest1/PartC/QC2/Parser.java
@@ -11,65 +11,55 @@ public class Parser {
      *  <exp> ::= <integer literal> | (<exp>*<exp>) | (<exp>/<exp>) | !(<exp>)
      *
      *
-     *  <exp>  ::= <term> | (<term>*<term>) | (<term>/<term>) | !(<term>)
-     *  <term> ::= <integer literal> | (<exp>)
+     *  <exp> ::= <term>
+     *  <term> ::=  <factor> |  <factor> * <term> | <factor> / <term>
+     *  <factor> ::= <integer literal> | (<exp>) | !(<exp>)
      *
      */
-
 
     public Exp parseExp() {
         // TODO: Implement this method
         // You can use any standard library if you want
         // You can add any method if it helps implementation
 
-        Exp term1 = null;
-        Exp term2 = null;
-
-        // Factorial.
-        if (_tokenizer.current().type == TokenType.EXCLAMATION) {
-            _tokenizer.next();  // The next "(".
-            while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
-                _tokenizer.next();
-                term1 = parseExp();
-            }
-            _tokenizer.next();
-            return new FacExp(term1);
-
-        }
-        // Multiplication / Division.
-        else if (_tokenizer.current().type == TokenType.LEFTBRACKET) {
-            while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
-                _tokenizer.next();
-                term1 = parseTerm();
-
-                if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.MULTIPLY) {
-                    _tokenizer.next();
-                    term2 = parseTerm();
-                    term1 = new MultExp(term1, term2);
-                }
-
-                if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.DIVIDE) {
-                    _tokenizer.next();
-                    term2 = parseTerm();
-                    term1 = new DivExp(term1, term2);
-                }
-            }
-            _tokenizer.next();
-            return term1;
-        }
-        // Integer literal
-        else {
-            return parseTerm();
-        }
+        return parseTerm();
     }
 
     public Exp parseTerm() {
+        Exp factor = parseFactor();
+
+        if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.MULTIPLY) {
+            _tokenizer.next();
+            Exp term = parseTerm();
+            return new MultExp(factor, term);
+
+        } else if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.DIVIDE) {
+            _tokenizer.next();
+            Exp term = parseTerm();
+            return new DivExp(factor, term);
+        } else {
+            return factor;
+        }
+    }
+
+    public Exp parseFactor() {
         Exp exp = null;
 
         if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.INT) {
             exp = new IntExp(_tokenizer.current().value);
             _tokenizer.next();
             return exp;
+        }
+        // Factorial.
+        else if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.EXCLAMATION) {
+            _tokenizer.next();  // The next "(".
+            while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
+                _tokenizer.next();
+                exp = parseExp();
+            }
+            _tokenizer.next();
+            return new FacExp(exp);
+
         } else if (_tokenizer.current().type == TokenType.LEFTBRACKET) {
             while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
                 _tokenizer.next();
--- comp2100-labs/labtest1/PartC/QC3/Parser.java
+++ comp2100-labs/labtest1/PartC/QC3/Parser.java
@@ -7,11 +7,74 @@ public class Parser {
         _tokenizer = tokeniser;
     }
 
+    /**
+     *  <BOOL> ::= NOT (<BOOL>) | (<BOOL> AND <BOOL>) | (<BOOL> OR <BOOL>) | TRUE | FALSE
+     *
+     *  <BOOL>   ::=  <factor> | <factor> AND <BOOL> | <factor> OR <BOOL>
+     *  <factor> ::= TRUE | FALSE | ( <BOOL> ) | NOT ( <BOOL> )
+     *
+     */
+
     public Bool parseBool() {
         // TODO: Implement this method
         // You can use any standard library if you want
         // You can add any method if it helps implementation
-        return null;
+
+        Bool bool = parseFactor();
+
+        if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.AND) {
+            _tokenizer.next();
+            Bool factor = parseFactor();
+            return new AndExp(bool, factor);
+        } else if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.OR) {
+            _tokenizer.next();
+            Bool factor = parseFactor();
+            return new OrExp(bool, factor);
+        } else {
+            return bool;
+        }
+    }
+
+    /**
+     *  <BOOL> ::= NOT (<BOOL>) | (<BOOL> AND <BOOL>) | (<BOOL> OR <BOOL>) | TRUE | FALSE
+     *
+     *  <BOOL>   ::=  <factor> | <factor> AND <BOOL> | <factor> OR <BOOL>
+     *  <factor> ::= TRUE | FALSE | ( <BOOL> ) | NOT ( <BOOL> )
+     *
+     */
+
+    public Bool parseFactor() {
+        Bool factor = null;
+
+        if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.TRUE) {
+            factor = new True();
+            _tokenizer.next();
+            return factor;
+
+        } else if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.FALSE) {
+            factor = new False();
+            _tokenizer.next();
+            return factor;
+
+        } else if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.NOT) {
+            _tokenizer.next(); // Now the token is left brace.
+            while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
+                _tokenizer.next();
+                factor = parseBool();
+            }
+            _tokenizer.next();
+            return new NotExp(factor);
+
+        } else if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.LEFTBRACKET) {
+            while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
+                _tokenizer.next();
+                factor = parseBool();
+            }
+            _tokenizer.next();
+            return factor;
+        } else {
+            return factor;
+        }
     }
 
 }
