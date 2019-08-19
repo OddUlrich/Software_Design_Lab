ed1f7b8d8e0ff0236a965d1b3fe7b5d34b3d1baa
OddUlrich <vmoonodd@gmail.com>
2019-08-19 20:09:51 +1000

    Finished before PartC QC2.

--- comp2100-labs/labtest1/PartC/QC2/Parser.java
+++ comp2100-labs/labtest1/PartC/QC2/Parser.java
@@ -7,11 +7,78 @@ public class Parser {
         _tokenizer = tokeniser;
     }
 
+    /**
+     *  <exp> ::= <integer literal> | (<exp>*<exp>) | (<exp>/<exp>) | !(<exp>)
+     *
+     *
+     *  <exp>  ::= <term> | (<term>*<term>) | (<term>/<term>) | !(<term>)
+     *  <term> ::= <integer literal> | (<exp>)
+     *
+     */
+
+
     public Exp parseExp() {
         // TODO: Implement this method
         // You can use any standard library if you want
-        // You can add any method if it helps implementation        
-        return null;
+        // You can add any method if it helps implementation
+
+        Exp term1 = null;
+        Exp term2 = null;
+
+        // Factorial.
+        if (_tokenizer.current().type == TokenType.EXCLAMATION) {
+            _tokenizer.next();  // The next "(".
+            while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
+                _tokenizer.next();
+                term1 = parseExp();
+            }
+            _tokenizer.next();
+            return new FacExp(term1);
+
+        }
+        // Multiplication / Division.
+        else if (_tokenizer.current().type == TokenType.LEFTBRACKET) {
+            while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
+                _tokenizer.next();
+                term1 = parseTerm();
+
+                if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.MULTIPLY) {
+                    _tokenizer.next();
+                    term2 = parseTerm();
+                    term1 = new MultExp(term1, term2);
+                }
+
+                if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.DIVIDE) {
+                    _tokenizer.next();
+                    term2 = parseTerm();
+                    term1 = new DivExp(term1, term2);
+                }
+            }
+            _tokenizer.next();
+            return term1;
+        }
+        // Integer literal
+        else {
+            return parseTerm();
+        }
     }
 
+    public Exp parseTerm() {
+        Exp exp = null;
+
+        if (_tokenizer.hasNext() && _tokenizer.current().type == TokenType.INT) {
+            exp = new IntExp(_tokenizer.current().value);
+            _tokenizer.next();
+            return exp;
+        } else if (_tokenizer.current().type == TokenType.LEFTBRACKET) {
+            while (_tokenizer.hasNext() && _tokenizer.current().type != TokenType.RIGHTBRACKET) {
+                _tokenizer.next();
+                exp = parseExp();
+            }
+            _tokenizer.next();
+            return exp;
+        } else {
+            return null;
+        }
+    }
 }
